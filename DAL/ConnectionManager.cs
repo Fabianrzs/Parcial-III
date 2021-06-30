033abb1790f83a18785ba8cd59e26dcecdc90772
Fdamirez <fdramirez@unicesar.edu.co>
2021-06-30 07:27:26 -0500

    Agregar archivos de proyecto.

---
+++ DAL/ConnectionManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class ConnectionManager
+    {
+        internal SqlConnection _connection;
+
+        public ConnectionManager(string connectionString)
+        {
+            _connection = new SqlConnection(connectionString);
+        }
+
+        public void Open()
+        {
+            _connection.Open();
+        }
+
+        public void Close()
+        {
+            _connection.Close();
+        }
+    }
+}
---
+++ Presentacion/ConfigConnection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentacion
+{
+    class ConfigConnection
+    {
+        public static string connectionString =
+            ConfigurationManager.ConnectionStrings
+            ["DefaultConnection"].ConnectionString;
+    }
+}
